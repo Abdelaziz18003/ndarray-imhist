68d10912ddbc2e678d18f8d3bdc0ac62c1657780
Abdelaziz Mokhnache <m.abdelaziz1993@gmail.com>
2019-10-14 17:03:00 +0100

    imhist y sum should equal height x width

--- test/index.js
+++ test/index.js
@@ -1,7 +1,5 @@
 const test = require('tape');
-const ndarray = require('ndarray');
 const getPixels = require('get-pixels');
-const pool = require('ndarray-scratch');
 const imhist = require('../index');
 
 test('Basic tests', (t) => {
@@ -9,15 +7,16 @@ test('Basic tests', (t) => {
     () => imhist([1, 2, 3], {plot: false}),
     /to be an ndarray/,
     'imhist of non ndarray inputs should throw'
-  )
-  t.end()
+  );
+  t.end();
 });
 
-test('Images tests', (t) => {
+test('Tests on lena_gray_256.png image', (t) => {
   getPixels('test/lena_gray_256.png', (err, pixels) => {
-    const [x1, y1] = imhist(pixels, {channel: 0, plot: false});
-    t.equal(x1.length, 256, 'X length should be 256');
-    t.equal(y1.length, 256, 'Y length should be 256');
+    const [x, y] = imhist(pixels, {channel: 0, plot: false});
+    t.equal(x.length, 256, 'X length should be 256');
+    t.equal(y.length, 256, 'Y length should be 256');
+    t.equal(y.reduce((a, b) => a + b), 65536, 'Y sum should be equal to 256x256');
     t.end();
   })
 });
